d07d518774fad55cd33b13bc0b6315193b57b226
Marcus Herstik <marcus@herstik.com>
2025-07-16 18:29:36 +1000

    Create T1Wk1.py

---
+++ T1Wk1.py
@@ -0,0 +1,25 @@
+# This is a comment
+##### This is another comment
+
+a = 1
+# a is a variable
+# It is storing a number
+
+b = 2.0
+# b is a variable
+# It is storing a number
+
+c = "xyz"   # string
+# c is also a variable.
+# It is storing a string.
+
+d = "3.2"  # It is storing a string.
+
+c = a + b
+# print(a + b)
+# print(b + float(d))
+# 
+# a = 4 # changed
+# print(a + b)
+# 
+
