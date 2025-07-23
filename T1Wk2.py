b1a8cd1f855c817af1fbdb7eafe36f6568c7e60e
mherstik <marcus@herstik.com>
2025-07-23 16:27:21 +1000

    Create T1Wk2.py

---
+++ T1Wk2.py
@@ -0,0 +1,16 @@
+# Input: marks
+
+
+# Output: letter grade or message (print)
+# if (marks > 100)
+#   print Out of range 0-100
+# else if (marks > 84) 
+#   print HD
+# else if (marks > 74) 
+#   print D
+# else if (marks> 64) 
+#   print C
+# else if (marks > 49) 
+#   print P
+# else
+#   print F 
