66e51fe14c9970011a5bd2289a944afdd160f7cd
Marcus Herstik <marcus@herstik.com>
2025-07-30 15:46:14 +1000

    Wk3 & update 2

--- T1Wk2.py
+++ T1Wk2.py
@@ -2,9 +2,9 @@
 
 marks = input("What was your score: ")
 
-print(type(marks))
+# print(type(marks))
 marks = float(marks)  # Convert to integer ??
-print(type(marks))
+# print(type(marks))
 
 # Output: letter grade or message (print)
 # if (marks > 100)
@@ -16,13 +16,15 @@ elif marks > 84:
     print("HD")
 #   print HD
 # else if (marks > 74) 
-#   print D
-# else if (marks> 64) 
-#   print C
-# else if (marks > 49) 
-#   print P
-# else
+elif marks > 74:
+    print("D")
+# else if (marks> 64)
+elif marks > 64:
+    print("C")
+# else if (marks > 49)
+elif marks > 49:
+    print("P")
 else:
     print("F")
-#   print F 
+
 
