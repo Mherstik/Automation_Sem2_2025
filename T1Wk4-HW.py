3686faee38a4a72ce75b2315058045a617085085
Marcus Herstik <marcus@herstik.com>
2025-08-06 18:14:04 +1000

    Create T1Wk4-HW.py

---
+++ T1Wk4-HW.py
@@ -0,0 +1,11 @@
+name = input("Give me a name: ")
+print(len(name))
+
+# Check to make sure the password has at least 10 characters
+# User has 3 attempts to put a valid password
+# Bonus - must have at least 1 uppercase, 1 lowercase & 1 number
+# Bonus Bonus - and 1 special character
+print(name.upper())
+print(name.lower())
+for each in name:
+    print(each)
