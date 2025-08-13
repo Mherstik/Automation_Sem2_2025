973564e473ce6734a1b761fa8c1b38975eaa7224
Marcus Herstik <marcus@herstik.com>
2025-08-13 16:29:49 +1000

    Update T1Wk4-HW.py

--- T1Wk4-HW.py
+++ T1Wk4-HW.py
@@ -5,7 +5,31 @@ print(len(name))
 # User has 3 attempts to put a valid password
 # Bonus - must have at least 1 uppercase, 1 lowercase & 1 number
 # Bonus Bonus - and 1 special character
-print(name.upper())
-print(name.lower())
-for each in name:
-    print(each)
+# print(name.upper())
+# print(name.lower())
+# for each in name:
+#     print(each)
+
+MAX_ATTEMPTS = 3
+
+for attempt in range(1, MAX_ATTEMPTS + 1):    
+    pword = input(f"{name}, give me a password: ")
+    if len(pword) >= 10:
+        print("Password meets minimum length")
+        break
+    else:
+        print("Password DOES NOT meet minimum length")
+        if attempt == MAX_ATTEMPTS:
+            print("Maximum attempts reached. Access denied!")
+        else:
+            print(f"{name} you have {MAX_ATTEMPTS - attempt} attempts left")
+    
+for char in pword:
+    if char.isupper():
+        pass
+    else:
+        print(f"{char} is not an upper character")
+        
+    
+    
+    
