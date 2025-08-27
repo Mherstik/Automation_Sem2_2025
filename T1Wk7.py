fe624b4b3ec5e6541299bd68e52b23a1bd36a5f4
Marcus Herstik <marcus@herstik.com>
2025-08-27 15:40:22 +1000

    Updating Week 7 files

--- T1Wk7.py
+++ T1Wk7.py
@@ -1,35 +1,5 @@
-x = 5
-y = input("Gimme a number: ")
-
-#z = int(z)
-#print(type(z))
-try:
-    y = int(y)
-    z = x/y
-    print(type(z))
-    print(z)
-    print("Division = ", round(z,2))
-except ZeroDivisionError:
-    print("You tried to divide by zero. \nWho do you think you are?")
-except ValueError:
-    print("Take a look at yourself")
-#except:
-#    print('Something went wrong!')
-
-
-c = 12
-if c == 12:
-    print("Assignment")
-else:
-    print("No assignment")
-
-#### Assertion test
-
-
-a = "Hello"
-assert a == "Hello" , "Tell them what went wrong"
-b = "Something"
-assert b == "Something", "Now it's hello"
-
-
+###
+# Convert Fahrenheit to Celcius
+###
+# C = (F - 32) × 5/9
 
