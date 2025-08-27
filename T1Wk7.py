fb2de45147117b827de8fb3b706f4aa05a8d527d
Marcus Herstik <marcus@herstik.com>
2025-08-27 15:28:20 +1000

    Update T1Wk7.py

--- T1Wk7.py
+++ T1Wk7.py
@@ -13,11 +13,18 @@ except ZeroDivisionError:
     print("You tried to divide by zero. \nWho do you think you are?")
 except ValueError:
     print("Take a look at yourself")
-except:
-    print('Something went wrong!')
+#except:
+#    print('Something went wrong!')
 
+
+c = 12
+if c == 12:
+    print("Assignment")
+else:
+    print("No assignment")
 
 #### Assertion test
+
 
 a = "Hello"
 assert a == "Hello" , "Tell them what went wrong"
