64d001769634c89f9e38dc099fd1e29600b50809
Marcus Herstik <marcus@herstik.com>
2025-08-27 16:13:04 +1000

    Update T1Wk7.py

--- T1Wk7.py
+++ T1Wk7.py
@@ -3,3 +3,31 @@
 ###
 # C = (F - 32) × 5/9
 
+def getUserTemp():
+    userDegree = input("What is the temperature in F: ")
+
+    while True:
+        try:
+            userDegree = int(userDegree)
+            return userDegree
+            break
+        except:
+            print("Please put a valid number.")
+            userDegree = input("What is the temperature in F: ")
+
+userTemp = getUserTemp()
+
+def convertFtoC(userTemp):
+    # C = (F - 32) × 5/9
+    celcius = (userTemp - 32) * 5 / 9
+    # celcius = round(celcius,2)
+    # celcius = round(((userTemp - 32) * 5 / 9),2)
+    print(f"{userTemp}F is equal to {celcius:.2f}C")
+
+def convertCtoF(userTemp):
+    pass
+
+###
+## Ask the user what they want to convert to or from
+###
+
