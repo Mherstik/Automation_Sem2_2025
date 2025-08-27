9580c83ad588aeda2038e161bd5a83557697d7ee
Marcus Herstik <marcus@herstik.com>
2025-08-27 17:03:12 +1000

    Update T1Wk7-HW.py

--- T1Wk7-HW.py
+++ T1Wk7-HW.py
@@ -2,7 +2,7 @@
 # Input a word and print it backwards.
 
 # 2. Find the largest number in a list
-# Use a list of numbers and find the maximum
+# Get a list of numbers and find the maximum
 
 # 3. Count vowels in a string
 # Input a sentence and count how many vowels it contains.
