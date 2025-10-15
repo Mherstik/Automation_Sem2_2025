b16bc490c71ebd33a9899abfb0af30ba45630ad3
Marcus Herstik <marcus@herstik.com>
2025-10-15 17:15:33 +1000

    Update T2Wk2.py

--- T2Wk2.py
+++ T2Wk2.py
@@ -3,7 +3,27 @@
 # Date: Term 4 2025
 # Purpose: To get speed of internet!
 
+
+## Check modules - import known good module
+import os			# interacting with operating system 
+import sys			# getting operating system information
+import subprocess	# spawn process and get results
+
+print(sys.executable)
+
 ## Import modules
+import importlib
+moduleList = ['requests','time', 'speedtest-cli']
+for each in moduleList:
+    spec = importlib.util.find_spec(each)
+    # print(spec)
+    if spec is not None:
+        print(f"Module {each} is found")
+    else:
+        print(f"Module {each} NOT found")
+        subprocess.check_call([sys.executable, '-m', 'pip', 'install', each])
+        print(f"Installed module {each}")
+
 from urllib.request import urlretrieve # built in to python
 import requests # requires install
 import time
@@ -33,7 +53,10 @@ starttime = time.time()
 # Scrapy
 
 # get file
-response = requests.get(file2download)
+# response = requests.get(file2download)
+
+## option 2 - using stream
+response = requests.get(file2download, stream=True)
 
 ### stop time
 endtime = time.time()
@@ -49,7 +72,18 @@ print(f"Download was {dlattempt}.") # \nResponse was {response.status_code}") #,
 print(f"Took {endtime - starttime} seconds")
 
 ### Calculate download speed
-speed = 50 * 8 /(endtime - starttime)
+speed = 50 * 8 / (endtime - starttime)
 print(f"{speed:.2f} Mbps")
+
+
+### Using URL retrieve
+print("Using url retrieve")
+start2 = time.time()
+urlretrieve(file2download)
+end2 = time.time()
+speed2 = 50 * 8 / (end2 - start2)
+print(f"Url retrieve took {end2 - start2:.2f} seconds, was {speed2:.2f} Mbps")
+
+
 
 
