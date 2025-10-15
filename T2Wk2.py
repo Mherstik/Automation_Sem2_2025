f346a140e03b74490f554418b184450f892d4b8e
Marcus Herstik <marcus@herstik.com>
2025-10-15 15:39:40 +1000

    Update T2Wk2.py

---
+++ T2Wk2.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+# Author: MarcusH
+# Date: Term 4 2025
+# Purpose: To get speed of internet!
+
+## Import modules
+from urllib.request import urlretrieve # built in to python
+import requests # requires install
+
+# PSEUDOCODE
+# 1) Get URL of file
+# 2) Start timer
+# 3) Download file
+# 4) Stop timer
+# 5) Calculate speed
+# 6) Save to file/variable for saving later
+
+file2download = "https://github.com/Mherstik/Automation_Sem2_2025/raw/refs/heads/main/50MB.zip"
+
+print("Starting download")
+print(f"Downloading file {file2download}")
+### start time
+
+
+### download file
+## urllib
+## requests
+## not recommended - advanced
+# BeautifulSoup
+# Scrapy
+
+# get file
+response = requests.get(file2download)
+
+# test if working
+if response.ok == True:
+    dlattempt = "successful"
+else: dlattempt = "failed"
+# print(response.status_code)
+
+### stop time
+print(f"Download was {dlattempt}.\nResponse was {response.status_code}, {response.ok}")
