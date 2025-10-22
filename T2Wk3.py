05359408a98fdd2b51b6c30a9c9eb87bca3fe7e5
Marcus Herstik <marcus@herstik.com>
2025-10-22 15:15:58 +1000

    Update T2Wk3.py

--- T2Wk3.py
+++ T2Wk3.py
@@ -14,6 +14,11 @@ import socket
 print(socket.gethostname())
 print(socket.gethostbyname(socket.gethostname()))
 
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.connect(("8.8.8.8", 80))
+print(s.getsockname()[0])
+
+
 
 print(os.getenv('COMPUTERNAME'))
 print(os.environ)
