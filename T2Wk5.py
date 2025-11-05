e5f31371444a2f83b94da7d24142296899b6a02a
Marcus Herstik <marcus@herstik.com>
2025-11-05 16:50:29 +1000

    Create T2Wk5.py

---
+++ T2Wk5.py
@@ -0,0 +1,11 @@
+### Get info from user
+# Name
+# Age
+# Postcode
+# Check to see if in file
+import csv
+
+name = input("What is your name: ")
+age = input("What is your age: ")
+postcode = input("What is your postcode: ")
+
